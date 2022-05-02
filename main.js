ac7787fd624c1fc8a3dbd3b67ef96cb634ca149f
std150670 <92640883+std150670@users.noreply.github.com>
2022-05-02 11:50:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,9 @@
 // these URLs come from Google Sheets 'shareable link' form
 // the first is the geometry layer and the second the points
 let geomURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsAyA0Hpk_-WpKyN1dfqi5IPEIC3rqEiL-uwElxJpw_U7BYntc8sDw-8sWsL87JCDU4lVg2aNi65ES/pub?output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRJu1cJLCmzDSw41MLPHAIsZmWbkVCRXFFT-5jucReDI-VnuuvFGqYbAe8fO3-chMmog8IeLFiBbkID/pub?output=csv";
 let pointsURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTTYEcPdq_REZ8UJ0mXMNsZ7FOIlad6PjRtGPo1g5vvdPqOwhPTYKxyUGR-IcoZk3d4aPVAcgjlBcpU/pub?output=csv";
 
 window.addEventListener("DOMContentLoaded", init);
 
